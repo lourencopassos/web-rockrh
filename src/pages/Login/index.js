3a1bb1d1e274ea4e7f46fa12ce689cf130ebce76
Lourenco <lo.passos93@gmail.com>
2021-07-20 22:00:41 -0300

    feat: create login page

---
+++ src/pages/Login/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div>
+      <p>Login</p>
+      <button>Sou funcionário</button>
+      <button>Sou gerente</button>
+      <div>
+        Não possui cadastro ainda?
+        <p>
+          Clique aqui
+        </p>
+      </div>
+    </div>
+  );
+}
