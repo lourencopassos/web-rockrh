13ca1d07fc79766085c21f2cf96cf268be66ed1c
Lourenco <lo.passos93@gmail.com>
2021-07-22 22:21:41 -0300

    feat: add signup page to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "../pages/Login";
+import SignUp from "../pages/Signup";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route component={Login} exact path="/" />
+        <Route component={SignUp} exact path="/cadastro" />
       </Switch>
     </BrowserRouter>
   );
