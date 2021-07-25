47dc84efb9c25cee302b361f3613eef98482b703
Lourenco <lo.passos93@gmail.com>
2021-07-25 19:03:35 -0300

    feat: add missing routes to application

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Login from "../pages/Login";
+import EmployeeDashboard from "../pages/EmployeeMain";
+import LoginEmployee from "../pages/LoginEmployee";
+import LoginManager from "../pages/LoginManager";
+
 import SignUp from "../pages/Signup";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route component={Login} exact path="/" />
+        <Route component={LoginEmployee} exact path="/funcionario/login" />
+        <Route component={EmployeeDashboard} exact path="/funcionario/dashboard" />
+        <Route component={LoginManager} exact path="/gerente/login" />
         <Route component={SignUp} exact path="/cadastro" />
       </Switch>
     </BrowserRouter>
