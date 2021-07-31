994396e730781811b3e9e05ec055349dad1b2b70
Lourenco <lo.passos93@gmail.com>
2021-07-31 14:14:46 -0300

    feat: add feedback info header

--- src/components/FeedbackInformationHeader/FeedbackInformationHeader.js
+++ src/components/FeedbackInformationHeader/FeedbackInformationHeader.js
@@ -1,27 +1,27 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
 
-export default function FeedbackInformationHeader() {
+export default function FeedbackInformationHeader({
+  evaluator,
+  company,
+  date,
+}) {
   return (
-    <Grid container spacing={3}>
-      <Grid container spacing={2}>
-        <Grid item xs={6} md={4} lg={3}>
-          <p>
-            Avaliador: <span>Carolina Kempfer</span>
-          </p>
-        </Grid>
-        <Grid item xs={6} md={3} lg={3}>
-          <p>
-            Empresa: <span>Doce Lar</span>
-          </p>
-        </Grid>
-        <Grid item xs={6} md={3} lg={3}>
-          <p>
-            Data de Avaliação: <span>30 Jul 2021</span>
-          </p>
-        </Grid>
+    <Grid container spacing={2}>
+      <Grid item xs={6} md={4} lg={3}>
+        <p>
+          Avaliador: <span>Carolina Kempfer</span>
+        </p>
       </Grid>
-      <Grid container spacing={2}>
+      <Grid item xs={6} md={3} lg={3}>
+        <p>
+          Empresa: <span>Doce Lar</span>
+        </p>
+      </Grid>
+      <Grid item xs={6} md={3} lg={3}>
+        <p>
+          Data de Avaliação: <span>30 Jul 2021</span>
+        </p>
       </Grid>
     </Grid>
   );
