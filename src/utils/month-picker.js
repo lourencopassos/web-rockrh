678a97f4907f87d7e8263dd3d41ca0864cb14945
Lourenco <lo.passos93@gmail.com>
2021-08-01 18:59:22 -0300

    feat: add missing months in month picker

--- src/utils/month-picker.js
+++ src/utils/month-picker.js
@@ -2,6 +2,10 @@ export const monthPicker = (month) => {
   switch (month) {
     case 6:
       return "Julho";
+    case 7:
+      return "Agosto";
+    case 8:
+      return "Setembro";
     default:
       break;
   }
