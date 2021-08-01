e497a7037169490911fc45020c6b1a5d38f90557
Lourenco <lo.passos93@gmail.com>
2021-08-01 18:58:17 -0300

    feat: add feedback detail card

--- src/pages/FeedbackDetail/FeedbackDetail.js
+++ src/pages/FeedbackDetail/FeedbackDetail.js
@@ -1,19 +1,16 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
 import FeedbackCard from "../../components/FeedbackCard/FeedbackCard";
-import FeedbackInformationHeader from "../../components/FeedbackInformationHeader/FeedbackInformationHeader";
+import InformationHeader from "../../components/InformationHeader/InformationHeader";
 import PortalLayout from "../../layouts/portal.index";
 
 export default function FeedbackDetail() {
   return (
-    <PortalLayout title="Avaliação #1">
-      <FeedbackInformationHeader />
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={6} lg={6}>
-          <FeedbackCard />
-        </Grid>
-        <Grid item xs={12} md={6} lg={6}>
-          <FeedbackCard />
+    <PortalLayout title="Feedback #1">
+      <InformationHeader />
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={12} lg={12}>
+          <FeedbackCard feedback={[]} />
         </Grid>
       </Grid>
     </PortalLayout>
